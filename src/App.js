dc20c17bc3355733786f7f4bc75f7a063fbbfbea
ram verma <ramvrm5@gmail.com>
2023-09-26 16:30:30 +0530

    changes in app.js file

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 
 //import animate on scroll
 import Aos from "aos";
-
 import 'aos/dist/aos.css';
 
 //import components
 import Hero from "./components/Hero";
-
 import About from "./components/About";
-
 import Features from "./components/Features";
-
 import Testimonials from "./components/Testimonials";
-
 import CtaSection from "./components/CtaSection";
-
 import Footer from "./components/Footer";
 
 const App = () => {
